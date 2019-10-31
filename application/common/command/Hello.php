d28f1ed78dbade2ffbd884b764cacaa996ae0e09
li <731184841@qq.com>
2019-10-31 17:14:24 +0800

    项目第一次推送

---
+++ application/common/command/Hello.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Project: Hello.php
+ * User: admin
+ * Date: 2019/10/31
+ * Time: 15:08
+ */
+
+namespace app\common\command;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Argument;
+use think\console\input\Option;
+use think\console\Output;
+
+class Hello extends Command
+{
+    protected function configure()
+    {
+        $this->setName('hello')
+            ->addArgument('name', Argument::OPTIONAL, "your name")
+            ->addOption('city', null, Option::VALUE_REQUIRED, 'city name')
+            ->setDescription('Say Hello');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        $name = trim($input->getArgument('name'));
+        $name = $name ?: 'thinkphp';
+
+        if ($input->hasOption('city')) {
+            $city = PHP_EOL . 'From ' . $input->getOption('city');
+        } else {
+            $city = '';
+        }
+
+        $output->writeln("Hello," . $name . '!' . $city);
+    }
+}
